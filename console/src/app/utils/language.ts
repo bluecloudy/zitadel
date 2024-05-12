ea0f59e9eda473a79db1aa1e2d4be75ab1c51f1a
Giang Nguyen <tn.hoanggiang@gmail.com>
2024-05-12 16:23:37 +0700

    feat: add Vietnamese language support

--- console/src/app/utils/language.ts
+++ console/src/app/utils/language.ts
@@ -1,3 +1,3 @@
-export const supportedLanguages = ['de', 'en', 'es', 'fr', 'it', 'ja', 'pl', 'zh', 'bg', 'pt', 'mk', 'cs', 'ru', 'nl'];
-export const supportedLanguagesRegexp: RegExp = /de|en|es|fr|it|ja|pl|zh|bg|pt|mk|cs|ru|nl/;
+export const supportedLanguages = ['de', 'en', 'es', 'fr', 'it', 'ja', 'pl', 'zh', 'bg', 'pt', 'mk', 'cs', 'ru', 'nl', 'vi'];
+export const supportedLanguagesRegexp: RegExp = /de|en|es|fr|it|ja|pl|zh|bg|pt|mk|cs|ru|nl|vi/;
 export const fallbackLanguage: string = 'en';
